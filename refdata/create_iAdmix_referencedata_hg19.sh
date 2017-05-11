21306e97941ffc2fd0299f501c8577bf8e919feb
JIHOON KIM <j5kim@ucsd.edu>
2017-05-11 10:42:42 -0700

    add linebreak for a better readability

--- refdata/create_iAdmix_referencedata_hg19.sh
+++ refdata/create_iAdmix_referencedata_hg19.sh
@@ -27,7 +27,9 @@ gunzip ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz
 # select single nucleotide polymorphisms (SNPs) to create iAdmix compatible reference data
 #              #chrom position rsid A1 A2 EAS SAS AFR EUR AMR 
 #              1 566875 rs2185539 T C 0.07 0.02 0.04 0.63 0.02
-echo -e "#chrom position rsid A1 A2 AMR AFR EUR SAS EAS"                            > iAdmix.reference.1000Gphase3snp.hg19.txt 
-grep "VT=SNP" ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf  | 
-     awk '{ split($8, a, ";"); print $1,$2,$3,$4,$5,a[6],a[7],a[8],a[9],a[10] }' |
-     sed 's/AMR_AF=//g;s/AFR_AF=//g;s/EUR_AF=//g;s/SAS_AF=//g;s/EAS_AF=//g'        >> iAdmix.reference.1000Gphase3snp.hg19.txt 
+echo -e "#chrom position rsid A1 A2 AMR AFR EUR SAS EAS"       | \
+     >  iAdmix.reference.1000Gphase3snp.hg19.txt 
+grep "VT=SNP" ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf  | \
+     awk '{ split($8, a, ";"); print $1,$2,$3,$4,$5,a[6],a[7],a[8],a[9],a[10] }' | \
+     sed 's/AMR_AF=//g;s/AFR_AF=//g;s/EUR_AF=//g;s/SAS_AF=//g;s/EAS_AF=//g'      | \
+     >> iAdmix.reference.1000Gphase3snp.hg19.txt 
