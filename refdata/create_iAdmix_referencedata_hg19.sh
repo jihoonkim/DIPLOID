d1e2f3a83b17b0c356672634cfab78fa827bdbbb
JIHOON KIM <j5kim@ucsd.edu>
2017-05-11 16:30:32 -0700

    add Python script to deal with randomly ordered allele frequencies in 1000G .vcf file

--- refdata/create_iAdmix_referencedata_hg19.sh
+++ refdata/create_iAdmix_referencedata_hg19.sh
@@ -14,22 +14,31 @@
 # Output: A whitespace-delimited .txt file with ten columns as below.
 #         #chrom position rsid A1 A2 AMR AFR EUR SAS EAS
 #         6 31245736 rs6906846 A G 0.7411 0.6787 0.6256 0.6899 0.6748
-#         American     AMR    
 #         African      AFR
+#         American     AMR    
+#         East Asian   EAS
 #         European     EUR
 #         South Asian  SAS
-#         East Asian   EAS
 #-----------------------------------------------------------------------------------
+export FTP_SITE="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/"
+export INPUT_VCF="ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf"
+export OUTPUT_TXT="iAdmix.reference.1000Gphase3snp.hg19.txt"
+
 # download a vcf file from 1000 Genomes Phase 3
-wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz 
-gunzip ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz
+#wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz 
+#gunzip ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz
+wget $FTP_SITE$INPUT_VCF
+gunzip $INPUT_VCF.gz
+
 
 # select single nucleotide polymorphisms (SNPs) to create iAdmix compatible reference data
 #              #chrom position rsid A1 A2 EAS SAS AFR EUR AMR 
 #              1 566875 rs2185539 T C 0.07 0.02 0.04 0.63 0.02
-echo -e "#chrom position rsid A1 A2 AMR AFR EUR SAS EAS"       | \
-     >  iAdmix.reference.1000Gphase3snp.hg19.txt 
-grep "VT=SNP" ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf  | \
-     awk '{ split($8, a, ";"); print $1,$2,$3,$4,$5,a[6],a[7],a[8],a[9],a[10] }' | \
-     sed 's/AMR_AF=//g;s/AFR_AF=//g;s/EUR_AF=//g;s/SAS_AF=//g;s/EAS_AF=//g'      | \
-     >> iAdmix.reference.1000Gphase3snp.hg19.txt 
+#echo -e "#chrom position rsid A1 A2 AFR AMR EAS EUR SAS"       | \
+#     >  iAdmix.reference.1000Gphase3snp.hg19.txt 
+#grep "VT=SNP" ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf  | \
+#     awk '{ split($8, a, ";"); print $1,$2,$3,$4,$5,a[6],a[7],a[8],a[9],a[10] }' | \
+#     sed 's/AMR_AF=//g;s/AFR_AF=//g;s/EUR_AF=//g;s/SAS_AF=//g;s/EAS_AF=//g'      | \
+#     >> iAdmix.reference.1000Gphase3snp.hg19.txt 
+echo -e "#chrom position rsid A1 A2 AFR AMR EAS EUR SAS"   > $OUTPUT_TXT
+python create_iAdmix_referencedata.py -i $INPUT_VCF          >> $OUTPUT_TXT
