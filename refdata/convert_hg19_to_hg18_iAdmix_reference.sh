8de584d9f4759e91bfbf2d3c17f37b898e97b2a6
JIHOON KIM <j5kim@ucsd.edu>
2017-05-22 16:14:31 -0700

    fix the error in header

--- refdata/convert_hg19_to_hg18_iAdmix_reference.sh
+++ refdata/convert_hg19_to_hg18_iAdmix_reference.sh
@@ -26,7 +26,7 @@ export REF_CHAIN=hg19ToHg18.over.chain
 
 # Convert iAdmix reference .txt format file (produced by running 
 #         create_iAdmix_referencedata_hg19.sh) to .bed format
-awk '{  OFS="\t"; if(NR > 1) print "chr"$1,$2-1,$2,$3";"$4";"$5";"$6";"$7";"$8";"$9";"$10 }' \
+awk '{  OFS="\t"; if(NR > 1) print "chr"$1,$2-1,$2,$3";"$4";"$5";"$6";"$7";"$8";"$9";"$10";"$11";"$12";"$13 }' \
     $IN_FILE  > $IN_FILE.bed 
 
 # Map hg19 positions to hg38 positions using liftOver and .over.chain file  
@@ -39,7 +39,7 @@ awk '{  OFS="\t"; if(NR > 1) print "chr"$1,$2-1,$2,$3";"$4";"$5";"$6";"$7";"$8";
 liftOver $IN_FILE.bed $REF_CHAIN $OUT_FILE.bed $OUT_FILE.unmapped 
 
 # Convert .bed output file (generaed by liftOver) to iAdmix reference .txt format
-echo -e "#chrom position rsid A1 A2 AFR AMR EAS EUR SAS" > $OUT_FILE
-awk '{ split($4, a, ";"); print $1,$3,a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8] }' \
+echo -e "#chrom position rsid A1 A2 YRI CHB CHD TSI MKK LWK CEU JPT" > $OUT_FILE
+awk '{ split($4, a, ";"); print $1,$3,a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9],a[10],a[11] }' \
 	$OUT_FILE.bed | \
 	sed 's/^chr//g' >>  $OUT_FILE
