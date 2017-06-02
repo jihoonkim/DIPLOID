c67c6246f6fde9764ecd29a56132a4df5b4141f8
maxabruzzi <maxabruzzi@gmail.com>
2017-06-02 10:55:41 -0700

    change the order of some fields in a few tables
    The order of the fields in the tables are made to be the same as those in the corresponding files to be imported.

--- affySNP6Create.sql
+++ affySNP6Create.sql
@@ -1,28 +1,31 @@
 --Create the affySNP6 database for birdseed to ped file conversion--
---
+CREATE DATABASE IF NOT EXISTS affySNP6;
+SHOW DATABASES;
+USE affySNP6;
 
-
-CREATE TABLE affyProbe(
+CREATE TABLE IF NOT EXISTS affyProbe(
 probeID varchar(45) NOT NULL,
 alleleA char(1) null default null,
 alleleB char(1) null default null,
 PRIMARY KEY(probeID)
 );
 
-CREATE TABLE rsidProbeMap(
+CREATE TABLE IF NOT EXISTS rsidProbeMap(
 rsid varchar(45) NOT NULL,
 probeID varchar(45) NOT NULL,
 PRIMARY KEY(probeID)
 );
 
-CREATE TABLE hg18Map(
-rsid varchar(45) NOT NULL,
+--DROP TABLE if exists hg18MAP;
+CREATE TABLE IF NOT EXISTS hg18Map(
 chrNumber char(2) not null,
+rsid varchar(45) NOT NULL,
 geneticDist float not null,
+location int not null,
 PRIMARY KEY(rsid)
 );
 
-CREATE TABLE metaInfo(
+CREATE TABLE IF NOT EXISTS metaInfo(
 race varchar(45) NOT NULL,
 ethinicity varchar(45) NOT NULL,
 caseID varchar(45) not null,
@@ -33,7 +36,7 @@ fileID varchar(45) not null,
 PRIMARY KEY(fileID)
 );
 
-CREATE TABLE birdSeed(
+CREATE TABLE IF NOT EXISTS birdSeed(
 caseID varchar(45) NOT NULL,
 probeID varchar(45) NOT NULL,
 calling int NOT NULL,
