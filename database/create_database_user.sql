bf4e818834c8b61bb6c62a64a6228e98bcf38939
JIHOON KIM <j5kim@ucsd.edu>
2017-07-10 10:22:16 -0700

    add tables

--- database/create_database_user.sql
+++ database/create_database_user.sql
@@ -2,13 +2,19 @@ CREATE DATABASE diploid;
 
 USE diploid;
 
-# mgrpasswd
+# database manater
 CREATE USER 'mgr'@'localhost' IDENTIFIED BY 'mgrpasswd';
-
 GRANT ALL PRIVILEGES ON diploid. * TO 'mgr'@'localhost';
-
-
+GRANT FILE ON *.* to 'mgr'@'localhost';
 FLUSH PRIVILEGES;
+
+
+# database user
+CREATE USER 'caddy'@'localhost' IDENTIFIED BY 'actri';
+GRANT ALL PRIVILEGES ON diploid. * TO 'mgr'@'localhost';
+GRANT FILE ON *.* to 'caddy'@'localhost';
+FLUSH PRIVILEGES;
+
 
 
 mysql -D diploid -u mgr -p 
--- database/optimize_mariadb.sh
+++ database/optimize_mariadb.sh
@@ -1,1 +1,5 @@
 https://mariadb.com/kb/en/mariadb/server-system-variables/#thread_cache_size
+
+
+
+--bulk-insert-buffer-size=
