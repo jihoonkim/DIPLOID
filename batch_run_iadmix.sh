f56f566d96115d5d73083287d7b3f7c872610937
JIHOON KIM <j5kim@ucsd.edu>
2017-07-10 17:42:51 -0700

    add parsing iAdmix outputs to create a single output file

--- batch_run_iadmix.sh
+++ batch_run_iadmix.sh
@@ -8,7 +8,12 @@ export OUTPUT_DIR=/scratch/PLINK/hapmap3_8populations_hg18
 for i in *.ped
 do
   PREFIX=`basename $i .ped`
-  ### compute population allele frequencies 
+  # compute population allele frequencies 
   python    ${iADMIX_DIR}/runancestry.py  --freq=${RESOURCE_DATA} --cores=4 \
     --path=${iADMIX_DIR} --plink=${i} --out=${OUTPUT_DIR}/${i} 
 done
+
+
+echo -e "reference sample YRI CHB CHD TSI MKK LWK CEU JPT" > af.hapmap3_8populations_hg18.txt 
+grep -w "final maxval" *.input.ancestry | awk -F ':' '{split($1,a,"."); print a[1],a[2], $3,$4,$5,$6,$7,$8,$9,$10}' | \
+  cut -d ' ' -f1,2,3,5,7,9,11,13,15,17  >> af.hapmap3_8populations_hg18.txt
