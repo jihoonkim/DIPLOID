dcdca2dd4d85a0c4d566dff0947a66b983503c03
JIHOON KIM <j5kim@ucsd.edu>
2017-07-10 18:15:01 -0700

    comment out post-run file gathering

--- batch_run_iadmix.sh
+++ batch_run_iadmix.sh
@@ -13,7 +13,6 @@ do
     --path=${iADMIX_DIR} --plink=${i} --out=${OUTPUT_DIR}/${i} 
 done
 
-
-echo -e "reference sample YRI CHB CHD TSI MKK LWK CEU JPT" > af.hapmap3_8populations_hg18.txt 
-grep -w "final maxval" *.input.ancestry | awk -F ':' '{split($1,a,"."); print a[1],a[2], $3,$4,$5,$6,$7,$8,$9,$10}' | \
-  cut -d ' ' -f1,2,3,5,7,9,11,13,15,17  >> af.hapmap3_8populations_hg18.txt
+# echo -e "reference sample YRI CHB CHD TSI MKK LWK CEU JPT" > af.hapmap3_8populations_hg18.txt 
+# grep -w "final maxval" *.input.ancestry | awk -F ':' '{split($1,a,"."); print a[1],a[2], $3,$4,$5,$6,$7,$8,$9,$10}' | \
+#  cut -d ' ' -f1,2,3,5,7,9,11,13,15,17  >> af.hapmap3_8populations_hg18.txt
